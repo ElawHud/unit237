29393ef7c4e37a84f81ca25ddbd342e72694c110
ElawHud <101118586+ElawHud@users.noreply.github.com>
2022-03-14 18:21:33 +0000

    Add files via upload
    Code for assignment 1

---
+++ Server_assignement.py
@@ -0,0 +1,40 @@
+# import library
+import math, random
+
+import socket
+s = socket.socket() 
+
+port = 7777
+
+s.bind(('',port))
+print ("socket binded to %s" %(port))
+
+s.listen(5)
+print ("socket is listening ")
+
+
+
+# function to generate OTP
+def generateOTP() :
+ 
+    # Declare a digits variable 
+    # which stores all digits
+    digits = "0123456789"
+    OTP = ""
+ 
+   # length of password can be changed
+   # by changing value in range
+    for i in range(4) :
+        OTP += digits[math.floor(random.random() * 10)]
+ 
+    return OTP
+
+while True:
+    c, addr = s.accept()
+    print ("got connection from ", addr)
+    
+    c.send("thank you for connecting".encode())
+    
+    c.close()
+    
+    break
